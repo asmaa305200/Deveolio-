9205d4f3639f208422cfcd42ae6d288cbfc0898a
asmaa305200 <127359890+asmaa305200@users.noreply.github.com>
2024-01-06 11:47:48 +0200

    commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//window.addEventListener('scroll', function () {
+            //     var navbar = document.querySelector('.navbar');
+            //     if (window.scrollY > 160) { 
+            //         navbar.classList.remove('bg-transparent');
+            //         navbar.classList.add('bg-scroll');
+            //     } else {
+            //         navbar.classList.remove('bg-scroll');
+            //         navbar.classList.add('bg-transparent');
+            //     }
+            // });
